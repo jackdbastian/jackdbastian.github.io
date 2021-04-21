dc10944edd3d0ca5a59a8d660bd0167ffd6f90ac
jackdbastian <jackdbastian@gmail.com>
2021-04-20 19:07:57 -0700

    added date to bash

--- _assets/bash/apts_update.sh
+++ _assets/bash/apts_update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /Users/Jack/Documents/GitHub/jackdbastian.github.io/
+echo "$(date) ---------------------------"
 /Library/Frameworks/Python.framework/Versions/3.9/bin/python3 /Users/Jack/Documents/GitHub/jackdbastian.github.io/_assets/python/craigslist_apts.py
 git add /Users/Jack/Documents/GitHub/jackdbastian.github.io/_data/apts_json.json
 git add /Users/Jack/Documents/GitHub/jackdbastian.github.io/_data/apts_csv.csv
