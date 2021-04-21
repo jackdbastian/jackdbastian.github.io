9b6ac94ce41360743ddd1423213441dc43daddad
jackdbastian <jackdbastian@gmail.com>
2021-04-21 15:47:59 -0700

    added -C

--- _assets/bash/apts_update.sh
+++ _assets/bash/apts_update.sh
@@ -2,7 +2,7 @@
 cd /Users/Jack/Documents/GitHub/jackdbastian.github.io/
 echo "$(date) ---------------------------"
 /Library/Frameworks/Python.framework/Versions/3.9/bin/python3 /Users/Jack/Documents/GitHub/jackdbastian.github.io/_assets/python/craigslist_apts.py
-git add /Users/Jack/Documents/GitHub/jackdbastian.github.io/_data/apts_json.json
-git add /Users/Jack/Documents/GitHub/jackdbastian.github.io/_data/apts_csv.csv
-git commit -m "apt update"
-git push
+git -C /Users/Jack/Documents/GitHub/jackdbastian.github.io add /Users/Jack/Documents/GitHub/jackdbastian.github.io/_data/apts_json.json
+git -C /Users/Jack/Documents/GitHub/jackdbastian.github.io add /Users/Jack/Documents/GitHub/jackdbastian.github.io/_data/apts_csv.csv
+git -C /Users/Jack/Documents/GitHub/jackdbastian.github.io commit -m "apt update"
+git -C /Users/Jack/Documents/GitHub/jackdbastian.github.io push
