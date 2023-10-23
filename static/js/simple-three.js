37de72090098d222ff0aa80e533d27f4a536c263
Jack Bastian <jack@jacksmacstudio.lan>
2023-10-23 10:09:06 -0700

    change cube to blue

--- static/js/simple-three.js
+++ static/js/simple-three.js
@@ -4,7 +4,7 @@ function main() {
 	const renderer = new THREE.WebGLRenderer( { antialias: true, canvas } );
 
 	const fov = 75;
-	const aspect = 2; // the canvas default
+	const aspect = 1; // the canvas default
 	const near = 0.1;
 	const far = 5;
 	const camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
@@ -27,14 +27,31 @@ function main() {
 	const boxDepth = 1;
 	const geometry = new THREE.BoxGeometry( boxWidth, boxHeight, boxDepth );
 
-	const material = new THREE.MeshPhongMaterial( { color: 0x44aa88 } ); // greenish blue
+	const material = new THREE.MeshPhongMaterial( { color: blue} ); // greenish blue
 
 	const cube = new THREE.Mesh( geometry, material );
 	scene.add( cube );
 
+	function resizeRendererToDisplaySize(renderer) {
+		const canvas = renderer.domElement;
+		const width = canvas.clientWidth;
+		const height = canvas.clientHeight;
+		const needResize = canvas.width !== width || canvas.height !== height;
+		if (needResize) {
+			renderer.setSize(width, height, false);
+		}
+		return needResize;
+	}
+
 	function render( time ) {
 
 		time *= 0.001; // convert time to seconds
+
+		if (resizeRendererToDisplaySize(renderer)) {
+			const canvas = renderer.domElement;
+			camera.aspect = canvas.clientWidth / canvas.clientHeight;
+			camera.updateProjectionMatrix();
+		}
 
 		cube.rotation.x = time;
 		cube.rotation.y = time/2;
