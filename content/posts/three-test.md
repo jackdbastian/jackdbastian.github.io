2f74ee89704b90041dcceaa3cc6b5b242121040b
Jack Bastian <jack@jacksmacstudio.lan>
2023-10-22 21:32:35 -0700

    add new three test

--- content/posts/three-test.md
+++ content/posts/three-test.md
@@ -2,5 +2,4 @@
 title: Look at the cube
 description: Three.js Test
 ---
-this is sum words
 {{< three-test >}}
--- static/js/three-test.js
+++ static/js/three-test.js
@@ -11,7 +11,7 @@ const near = 0.1;
 const far = 5;
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 
-camera.position.z = 6;
+camera.position.z = 90;
 
 
 const scene = new THREE.Scene();
